6b2f4e137defd7113d62086cc2ebbc3e18ca684e
Cody Baker <cody@flickerbox.com>
2023-09-25 14:00:22 -0700

    Fix external url check

--- packages/utility/dist/index.js
+++ packages/utility/dist/index.js
@@ -301,6 +301,9 @@ function getInnerBlocks(clientId) {
 }
 
 function isExternalUrl(url) {
+  if (url.indexOf('#') === 0 || url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+    return false;
+  }
   return new URL(url).origin !== location.origin;
 }
 
--- packages/utility/src/is-external-url.js
+++ packages/utility/src/is-external-url.js
@@ -1,4 +1,8 @@
 function isExternalUrl( url ) {
+	if ( url.indexOf('#') === 0 || ( url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0 ) ) {
+		return false
+	}
+
 	return ( new URL( url ).origin !== location.origin )
 }
 
